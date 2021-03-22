ba000464c33bffa9bbbf27137db5527b77215950
mahmoud-tharwat <mahmoudtharwat909090@gmail.com>
2021-03-22 12:51:52 +0200

    Setup the java code in MainActivity for the login layout
    setup the next button and write TODO for what should we do in future to link the user data (student name & ID) with the preference setting
    (MainActivity.java).

--- app/src/main/java/com/example/android/explorationgpa/MainActivity.java
+++ app/src/main/java/com/example/android/explorationgpa/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.android.explorationgpa;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String LOG_TAG = MainActivity.class.getSimpleName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        TextView mNextButton = (TextView) findViewById(R.id.main_activity_next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                // TODO(1): Setup the preference to contain the student name and ID.
+                // TODO(2): Setup an intent to open the next activity.
+
+            }
+        });
+
     }
+
 }
