d7af0b5c29b283956157127ea506b5484546ab0f
mahmoud-tharwat <mahmoudtharwat909090@gmail.com>
2021-03-24 15:58:02 +0200

    Link between the login layout and the preference settings
    insert the student name and id to the preference settings

--- app/src/main/java/com/example/android/explorationgpa/MainActivity.java
+++ app/src/main/java/com/example/android/explorationgpa/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.android.explorationgpa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -13,13 +16,14 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     public static final String LOG_TAG = MainActivity.class.getSimpleName();
-
+    private SharedPreferences mSharedPrefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
         final LinearLayout linearLayout = (LinearLayout) findViewById(R.id.activity_main_full_layout);
         final ProgressBar progressBar = (ProgressBar) findViewById(R.id.activity_main_progress_bar);
 
@@ -35,6 +39,9 @@ public class MainActivity extends AppCompatActivity {
                 linearLayout.setVisibility(View.GONE);
                 progressBar.setVisibility(View.VISIBLE);
 
+                // link the data that the user insert it to the preference settings
+                setPreference();
+
                 // destroy the current Activity and start the GpaActivity
                 Intent intent = new Intent(MainActivity.this , GpaActivity.class);
                 finish();
@@ -42,6 +49,24 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+    }
+
+
+
+    private void setPreference() {
+
+        EditText studentNameEditText = (EditText) findViewById(R.id.activity_main_student_name);
+        String studentName = studentNameEditText.getText().toString();
+
+        EditText studentIdEditText = (EditText) findViewById(R.id.activity_main_student_id);
+        String studentId = studentIdEditText.getText().toString();
+
+        // save the student name and the id in preference settings
+        SharedPreferences.Editor editor = mSharedPrefs.edit();
+        editor.putString(getString(R.string.settings_student_name_key), studentName);
+        editor.putString(getString(R.string.settings_student_id_key), studentId);
+        editor.apply();
 
     }
 
