b4c15a05535ca2cf07a72560f9032de0b06af9d7
mahmoud-tharwat <mahmoudtharwat909090@gmail.com>
2021-03-24 14:58:49 +0200

    Add menu file for the GpaActivity
    add menu contain settings option called menu_gpa.xml

--- app/src/main/java/com/example/android/explorationgpa/GpaActivity.java
+++ app/src/main/java/com/example/android/explorationgpa/GpaActivity.java
@@ -3,6 +3,8 @@ package com.example.android.explorationgpa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class GpaActivity extends AppCompatActivity {
 
@@ -11,4 +13,36 @@ public class GpaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gpa);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // for inflate the menu that we made
+        getMenuInflater().inflate(R.menu.menu_gpa, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.action_settings:
+                // TODO: start setting activity.
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+
+        // in case you want make change in the menu bar.
+
+        return true;
+    }
+
+
 }
