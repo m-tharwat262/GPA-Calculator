d59166e6a8159e902dcc80deff348ca2978f2ebd
mahmoud-tharwat <mahmoudtharwat909090@gmail.com>
2021-03-27 15:21:59 +0200

    Add Floating Action button to GpaActivity
    add a new library : 'com.google.android.material:material:1.3.0'
    setup the (java - xml) files for that.

--- app/src/main/java/com/example/android/explorationgpa/GpaActivity.java
+++ app/src/main/java/com/example/android/explorationgpa/GpaActivity.java
@@ -8,12 +8,16 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.example.android.explorationgpa.settings.SettingsActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class GpaActivity extends AppCompatActivity {
 
     private SharedPreferences mSharedPrefs;
+    private FloatingActionButton mFab;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +25,18 @@ public class GpaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_gpa);
 
         mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        mFab = (FloatingActionButton) findViewById(R.id.activity_gpa_fab);
 
         checkPreference();
+
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            // TODO: intent to open the next activity.
+
+            }
+        });
 
     }
 
@@ -40,7 +54,7 @@ public class GpaActivity extends AppCompatActivity {
         switch (item.getItemId()) {
 
             case R.id.action_settings:
-                // TODO: start setting activity.
+                // start setting activity.
                 Intent intent = new Intent(GpaActivity.this , SettingsActivity.class);
                 startActivity(intent);
                 return true;
