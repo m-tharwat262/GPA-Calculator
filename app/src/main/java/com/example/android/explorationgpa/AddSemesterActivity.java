20b32cdfe9208d3e7c16cf42cab8a8eb2c02cb3e
mahmoud-tharwat <mahmoudtharwat909090@gmail.com>
2021-04-10 08:46:51 +0200

    make the AddSemesterActivity display the year and term name in the layout

--- app/src/main/java/com/example/android/explorationgpa/AddSemesterActivity.java
+++ app/src/main/java/com/example/android/explorationgpa/AddSemesterActivity.java
@@ -10,28 +10,28 @@ import android.widget.TextView;
 
 public class AddSemesterActivity extends AppCompatActivity {
 
-    private static final String LOG_TAG = AddSemesterActivity.class.getSimpleName();
+    private static final String LOG_TAG = AddSemesterActivity.class.getSimpleName(); // activity name.
 
-    private TextView nameTextView; // for the basic info part in the layout
-    private TextView idTextView; // for the basic info part in the layout
-    private TextView semesterTextView; // for the basic info part in the layout
+    private TextView nameTextView; // for the basic info part in the layout.
+    private TextView idTextView; // for the basic info part in the layout.
+    private TextView semesterTextView; // for the basic info part in the layout.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_semester);
 
-        // get the specific view from the xml layout
+        // get the specific view from the xml layout.
         nameTextView = (TextView) findViewById(R.id.activity_add_semester_name);
         idTextView = (TextView) findViewById(R.id.activity_add_semester_id);
         semesterTextView = (TextView) findViewById(R.id.activity_add_semester_number_of_semester);
 
-        // get the information which the intent (from infoActivity) that open that activity
+        // get the information which the intent (from infoActivity) that open that activity.
         Intent intent = getIntent();
-        String studentName = intent.getStringExtra("student_name"); // for student name
-        int studentId = intent.getIntExtra("student_id",0); // for student id
-        int yearNumber = intent.getIntExtra("year_number", 0); // for number of the year
-        int termNumber = intent.getIntExtra("term_number", 0); // for number of the term
+        String studentName = intent.getStringExtra("student_name"); // for student name.
+        int studentId = intent.getIntExtra("student_id",0); // for student id.
+        int yearNumber = intent.getIntExtra("year_number", 0); // for number of the year.
+        int termNumber = intent.getIntExtra("term_number", 0); // for number of the term.
         Log.i(LOG_TAG, "intent come from InfoActivity contain : " + studentName + "  " + studentId + " " + yearNumber + " " + termNumber);
 
 
@@ -39,9 +39,11 @@ public class AddSemesterActivity extends AppCompatActivity {
         // show the user basic data on the top of the layout.
         setupTheBasicInfo(studentName, studentId, yearNumber, termNumber);
 
-
+        // show the year and term name in the layout.
+        setupTheYearAndTerm(yearNumber, termNumber);
 
     }
+
 
 
     /**
@@ -64,4 +66,58 @@ public class AddSemesterActivity extends AppCompatActivity {
         semesterTextView.setText(String.valueOf(semester));
 
     }
+
+
+    /**
+     * display the exact year and term name in the layout.
+     *
+     * @param yearNumber number of the year that the user want calculate the gpa in.
+     * @param termNumber number of the term that the user want calculate the gpa in.
+     */
+    private void setupTheYearAndTerm(int yearNumber, int termNumber) {
+
+        int yearNameResourceId;
+        int termNameResourceId;
+
+        // to get the resource id for the year name by the know the year number.
+        switch (yearNumber) {
+            case 1:
+                yearNameResourceId = R.string.year_one;
+                break;
+            case 2:
+                yearNameResourceId = R.string.year_two;
+                break;
+            case 3:
+                yearNameResourceId = R.string.year_three;
+                break;
+            case 4:
+                yearNameResourceId = R.string.year_four;
+                break;
+            default:
+                yearNameResourceId = R.string.year_zero;
+
+        }
+
+        // display the year name in the layout.
+        String yearName = getString(yearNameResourceId);
+        TextView yearNameTextView = (TextView) findViewById(R.id.activity_add_semester_year_name);
+        yearNameTextView.setText(yearName);
+
+
+        // to get the resource id for the term name by the know the term number.
+        if (termNumber == 1) {
+            termNameResourceId = R.string.term_first;
+        } else {
+            termNameResourceId = R.string.term_second;
+        }
+
+        // display the term name inside bracts in the layout.
+        String termName = getString(termNameResourceId);
+        String termNameInBracts = " (" + termName + ")";
+        TextView termNameTextView = (TextView) findViewById(R.id.activity_add_semester_term_name);
+        termNameTextView.setText(termNameInBracts);
+
+    }
+
+
 }
