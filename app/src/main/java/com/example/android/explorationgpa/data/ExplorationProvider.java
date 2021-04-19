1cbdfe1748ffe31e7839354d9e3fa74f04a78627
mahmoud-tharwat <mahmoudtharwat909090@gmail.com>
2021-04-20 01:27:22 +0200

    Setup query method inside ExplorationProvider.java to handle the read process from the semester database

--- app/src/main/java/com/example/android/explorationgpa/data/ExplorationProvider.java
+++ app/src/main/java/com/example/android/explorationgpa/data/ExplorationProvider.java
@@ -1,10 +1,13 @@
 package com.example.android.explorationgpa.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+import com.example.android.explorationgpa.data.ExplorationContract.SemesterGpaEntry;
 
 
 public class ExplorationProvider extends ContentProvider {
@@ -41,11 +44,64 @@ public class ExplorationProvider extends ContentProvider {
     }
 
 
+    /**
+     * Read from the database (all the database or a single row).
+     *
+     * @param uri uri for the path in the database.
+     * @param projection specific column in the database.
+     * @param selection specific row in the database.
+     * @param selectionArgs the value for the selection parameter above.
+     * @param sortOrder the order that the results must return with.
+     *
+     * @return Cursor contain the results after the reading from database finish.
+     */
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
 
-        // TODO : handle it.
-        return null;
+        // access to the semester database to read the data from it.
+        SQLiteDatabase semesterDatabase = mSemesterDbHelper.getReadableDatabase();
+
+        // for the return result from the database.
+        Cursor cursor;
+
+        // get the pattern that the uri equal.
+        int match = sUriMatcher.match(uri);
+
+        // setup functions for every uri pattern.
+        switch (match) {
+
+            // semester_gpa database with no id.
+            case SEMESTER_GPA:
+
+                // setup the input inside the query function.
+                cursor = semesterDatabase.query(SemesterGpaEntry.TABLE_NAME, projection,
+                        selection, selectionArgs, null, null, sortOrder);
+
+                break;
+
+            // semester_gpa database with id.
+            case SEMESTER_GPA_ID:
+
+                // setup the input inside the query function (after the WHERE word).
+                selection = SemesterGpaEntry._ID + "=?";
+                selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
+
+                cursor = semesterDatabase.query(SemesterGpaEntry.TABLE_NAME, projection, selection, selectionArgs,
+                        null, null, sortOrder);
+
+                break;
+
+            // to handle if the is no match for the uri inserted with the uri patterns.
+            default:
+
+                throw new IllegalArgumentException("Cannot query unknown URI " + uri);
+
+        }
+
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+
+        // return Cursor contain the data from the database.
+        return cursor;
     }
 
 
