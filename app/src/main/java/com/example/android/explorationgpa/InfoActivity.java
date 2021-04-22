03b4182d1d92b7209a85a221fae0edddbe216c0b
mahmoud-tharwat <mahmoudtharwat909090@gmail.com>
2021-04-22 09:26:39 +0200

    Update in LoginActivity.java by add a comments to the code and order it to make it easily for reading

--- app/src/main/java/com/example/android/explorationgpa/InfoActivity.java
+++ app/src/main/java/com/example/android/explorationgpa/InfoActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.explorationgpa;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -12,44 +13,62 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+
 
 public class InfoActivity extends AppCompatActivity {
 
 
-    private static final String LOG_TAG = InfoActivity.class.getSimpleName();
+    private static final String LOG_TAG = InfoActivity.class.getSimpleName(); // class name.
 
-    private EditText mNameEditText, mIdEditText;
+    private EditText mNameEditText, mIdEditText; // for student name & id.
 
-    private Spinner mLevelSpinner;
-    private ArrayAdapter levelSpinnerAdapter;
+    private Spinner mLevelSpinner; // for year names.
 
-    private RadioGroup mRadioGroup;
-    private RadioButton mFirstSemester, mSecondSemester;
+    private RadioGroup mRadioGroup;  // for term names.
 
-    private int mYear;
-    private int mTerm;
+    private int mYear; // refer to the number of the year that the user selected.
+    private int mTerm; // refer to the term of the year that the user selected.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
 
+
         mNameEditText = (EditText) findViewById(R.id.activity_info_student_name);
         mIdEditText = (EditText) findViewById(R.id.activity_info_student_id);
         mLevelSpinner = (Spinner) findViewById(R.id.activity_info_spinner_year);
         mRadioGroup = (RadioGroup) findViewById(R.id.activity_info_radio_buttons);
-        mFirstSemester = (RadioButton) findViewById(R.id.activity_info_radio_first);
-        mSecondSemester = (RadioButton) findViewById(R.id.activity_info_radio_second);
 
 
+        // display a spinner contain the year numbers (0-1-2-3-4-5).
+        // handle clicks on any item in the spinner.
         setupYearSpinner();
+
+        // handle clicks on the two radio buttons.
+        // know which one the user choose.
         setupSemesterRadio();
+
+        // access to the preference settings.
+        // insert the student name & id from preference settings inside the EditText automatically.
         checkPreferences();
 
-        // click on the next button send intent to open the AddSemesterActivity
+        // handle clicks on the NEXT button.
+        setupNextButton();
+
+
+    }
+
+
+
+    /**
+     * Handle clicks on the NEXT button.
+     */
+    private void setupNextButton() {
+
+        // clicking on the next button send intent to open the AddSemesterActivity.
         Button nextButton = (Button) findViewById(R.id.activity_info_next_button);
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,29 +76,31 @@ public class InfoActivity extends AppCompatActivity {
 
                 // open the AddSemesterActivity
                 Intent intent = new Intent(InfoActivity.this, AddSemesterActivity.class);
-                intent = makeContentOfIntent(intent); // get the intent contain the info which will be send
+                intent = makeContentsOfIntent(intent); // get the intent contain the info which will be send.
                 startActivity(intent);
-                finish();
+                finish(); // close the activity and clear its data in the ram.
 
             }
         });
+
     }
 
+
     /**
-     * setup the spinner fot years (0-1-2-3-4) and know which year the user select
+     * setup the spinner fot years (0-1-2-3-4) and know which year the user selected.
      */
     private void setupYearSpinner() {
 
-        levelSpinnerAdapter = ArrayAdapter.createFromResource(this,
+        ArrayAdapter levelSpinnerAdapter = ArrayAdapter.createFromResource(this,
                 R.array.array_level_options, android.R.layout.simple_spinner_item);
 
-        // Specify dropdown layout style - simple list view with 1 item per line
+        // Specify dropdown layout style - simple list view with 1 item per line.
         levelSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
 
-        // Apply the adapter to the spinner
+        // Apply the adapter to the spinner.
         mLevelSpinner.setAdapter(levelSpinnerAdapter);
 
-        // Set the integer mSelected to the constant values
+        // Set the integer mSelected to the constant values.
         mLevelSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -99,7 +120,7 @@ public class InfoActivity extends AppCompatActivity {
                 }
             }
 
-            // Because AdapterView is an abstract class, onNothingSelected must be defined
+            // Because AdapterView is an abstract class, onNothingSelected must be defined.
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
                 mYear = 0;
@@ -108,9 +129,8 @@ public class InfoActivity extends AppCompatActivity {
     }
 
 
-
     /**
-     * setup the two radio buttons fot terms (1-2) and know which radio buttons the user select
+     * setup the two radio buttons fot terms (1-2) and know which radio buttons the user select.
      */
     private void setupSemesterRadio() {
 
@@ -119,7 +139,7 @@ public class InfoActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
 
-                // find which radio button is selected
+                // find which radio button is selected.
                 if(checkedId == R.id.activity_info_radio_first) {
                     mTerm = 1;
                 } else if (checkedId == R.id.activity_info_radio_second){
@@ -132,25 +152,25 @@ public class InfoActivity extends AppCompatActivity {
     }
 
 
-
     /**
-     * setup the EditText field to automatically write the user data (name and id) from the preference settings
+     * setup the EditText field to automatically write the user data (name and id) from
+     * the preference settings.
      */
     private void checkPreferences () {
 
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
 
-        // get the student name from the preference settings
+        // get the student name from the preference settings.
         String studentNamePreference = sharedPrefs.getString(
                 getString(R.string.settings_student_name_key),
                 getString(R.string.settings_student_name_default));
 
-        // get the student id from the preference settings
+        // get the student id from the preference settings.
         String studentIdPreference = sharedPrefs.getString(
                 getString(R.string.settings_student_id_key),
                 getString(R.string.settings_student_id_default));
 
-        // make the EditTexts write the student name & id that saved in the prefernce
+        // make the EditTexts write the student name & id that saved in the preference.
         mNameEditText.setText(studentNamePreference);
         mIdEditText.setText(studentIdPreference);
     }
@@ -160,23 +180,24 @@ public class InfoActivity extends AppCompatActivity {
      * put the information that should be send from that activity to the next one
      * like (student name & id - number of the year & term).
      *
-     * @param intent for the intent which will be send
+     * @param intent for the intent which will be send.
      *
-     * @return intent include information
+     * @return intent include information.
      */
-    private Intent makeContentOfIntent(Intent intent) {
+    private Intent makeContentsOfIntent(Intent intent) {
 
         String studentName = mNameEditText.getText().toString(); // get student name from EditText.
         String studentIdString = mIdEditText.getText().toString(); // get student id from EditText.
-        int studentId = Integer.parseInt(studentIdString); // convert the id form string to integer
+        int studentId = Integer.parseInt(studentIdString); // convert the id form string to integer.
 
-        // put the information which will be send inside the intent
-        intent.putExtra("student_name", studentName); // for student name
-        intent.putExtra("student_id", studentId); // for student id
-        intent.putExtra("year_number", mYear); // for number of the year
-        intent.putExtra("term_number",mTerm); // for number of the term
+        // put the information which will be send inside the intent.
+        intent.putExtra("student_name", studentName); // for student name.
+        intent.putExtra("student_id", studentId); // for student id.
+        intent.putExtra("year_number", mYear); // for number of the year.
+        intent.putExtra("term_number",mTerm); // for number of the term.
 
         return intent;
     }
+
 
 }
--- app/src/main/java/com/example/android/explorationgpa/LoginActivity.java
+++ app/src/main/java/com/example/android/explorationgpa/LoginActivity.java
@@ -15,34 +15,48 @@ import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
-    public static final String LOG_TAG = LoginActivity.class.getSimpleName();
-    private SharedPreferences mSharedPrefs;
+
+    public static final String LOG_TAG = LoginActivity.class.getSimpleName(); // activity name.
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        // handle clicks on the NEXT button.
+        setupNextButton();
+
+    }
+
+
+
+    /**
+     * Handle clicks on the NEXT button.
+     */
+    private void setupNextButton() {
+
+        // all the layout except the ProgressBar.
         final LinearLayout linearLayout = (LinearLayout) findViewById(R.id.activity_main_full_layout);
+
         final ProgressBar progressBar = (ProgressBar) findViewById(R.id.activity_main_progress_bar);
 
+        // know when the NEXT button clicked.
         TextView mNextButton = (TextView) findViewById(R.id.activity_main_next_button);
         mNextButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
 
-                // TODO(1): Setup the preference to contain the student name and ID.
-
-                // make only the progress bar visible
+                // make only the progress bar visible when the button click.
                 linearLayout.setVisibility(View.GONE);
                 progressBar.setVisibility(View.VISIBLE);
 
-                // link the data that the user insert it to the preference settings
+                // link the data that the user insert it to the preference settings.
                 setPreference();
 
-                // destroy the current Activity and start the GpaActivity
+                // destroy the current Activity and start the GpaActivity.
                 Intent intent = new Intent(LoginActivity.this , GpaActivity.class);
                 startActivity(intent);
                 finish();
@@ -53,21 +67,30 @@ public class LoginActivity extends AppCompatActivity {
     }
 
 
-
+    /**
+     * Save the student info (name & ID) inside the preference settings.
+     */
     private void setPreference() {
 
+        // to access to the preference settings.
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        // get the student name from EditText.
         EditText studentNameEditText = (EditText) findViewById(R.id.activity_main_student_name);
         String studentName = studentNameEditText.getText().toString();
 
+        // get the student ID from EditText.
         EditText studentIdEditText = (EditText) findViewById(R.id.activity_main_student_id);
         String studentId = studentIdEditText.getText().toString();
 
-        // save the student name and the id in preference settings
-        SharedPreferences.Editor editor = mSharedPrefs.edit();
+        // save the student name and the id in preference settings.
+        SharedPreferences.Editor editor = sharedPrefs.edit();
         editor.putString(getString(R.string.settings_student_name_key), studentName);
         editor.putString(getString(R.string.settings_student_id_key), studentId);
         editor.apply();
 
     }
+
+
 
 }
