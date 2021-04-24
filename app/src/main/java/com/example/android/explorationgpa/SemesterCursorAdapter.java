efd29d88bbeb64f3276e6c590d88e571328d7a2d
mahmoud-tharwat <mahmoudtharwat909090@gmail.com>
2021-04-24 21:46:17 +0200

    Make the SemesterCursorAdapter display the semester number from the database inside the semester item

--- app/src/main/java/com/example/android/explorationgpa/SemesterCursorAdapter.java
+++ app/src/main/java/com/example/android/explorationgpa/SemesterCursorAdapter.java
@@ -2,6 +2,7 @@ package com.example.android.explorationgpa;
 
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.database.Cursor;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,8 +18,13 @@ public class SemesterCursorAdapter extends CursorAdapter {
 
     public static final String LOG_TAG = SemesterCursorAdapter.class.getSimpleName(); // class name.
 
+    private Context mContext;
+
+
     public SemesterCursorAdapter(Context context, Cursor c) {
         super(context, c, 0);
+
+        mContext = context;
 
     }
 
@@ -44,11 +50,13 @@ public class SemesterCursorAdapter extends CursorAdapter {
         // determine the views inside the item.
         TextView studentNameTextView = (TextView) view.findViewById(R.id.semester_item_student_name);
         TextView studentIdTextView = (TextView) view.findViewById(R.id.semester_item_student_id);
+        TextView semesterNumberTextView = (TextView) view.findViewById(R.id.semester_item_semester_number);
 
 
         // get the column position inside the table in semester database.
         int studentNameColumnIndex = cursor.getColumnIndexOrThrow(SemesterGpaEntry.COLUMN_STUDENT_NAME);
         int studentIdColumnIndex = cursor.getColumnIndexOrThrow(SemesterGpaEntry.COLUMN_STUDENT_ID);
+        int semesterNumberColumnIndex = cursor.getColumnIndexOrThrow(SemesterGpaEntry.COLUMN_SEMESTER_NUMBER);
 
 
         // get the student name from the database and display it in the screen.
@@ -60,6 +68,13 @@ public class SemesterCursorAdapter extends CursorAdapter {
         int studentId = cursor.getInt(studentIdColumnIndex);
         String studentIdAsString = String.valueOf(studentId); // convert student ID from Integer to String.
         studentIdTextView.setText(studentIdAsString);
+
+
+        // get the semester number from the database and display it in the screen.
+        int semesterNumber = cursor.getInt(semesterNumberColumnIndex);
+        Resources resources = mContext.getResources();
+        String semester = String.format(resources.getString(R.string.semester_with_placeholder), semesterNumber);
+        semesterNumberTextView.setText(semester);
 
     }
 
