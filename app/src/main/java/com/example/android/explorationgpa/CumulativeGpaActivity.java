1c19a1958f7f71e9ff45c595c951eff4c87c8455
mahmoud-tharwat <mahmoudtharwat909090@gmail.com>
2021-05-01 16:58:17 +0200

    Add a new activity called CumulativeGpaActivity that will responsible to display the student Cumulative GPA

---
+++ app/src/main/java/com/example/android/explorationgpa/CumulativeGpaActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.explorationgpa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CumulativeGpaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cumulative_gpa);
+    }
+}
