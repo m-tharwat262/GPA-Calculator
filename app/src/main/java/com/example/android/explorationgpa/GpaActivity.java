292228bdaa034aeae0ea6464bc198ccb4d4a5579
mahmoud-tharwat <mahmoudtharwat909090@gmail.com>
2021-05-01 23:46:15 +0200

    Check the validate of the semester uris before send it to CumulativeGpaActivity
    validate by :
    make it not allow to send less than two semester uri.
    make it not allow to send more than ten semester uri.
    make it not allow to send same semester number to CumulativeGpaActivity.
    add toast messages to hint the user about the validate above.

--- app/src/main/java/com/example/android/explorationgpa/GpaActivity.java
+++ app/src/main/java/com/example/android/explorationgpa/GpaActivity.java
@@ -23,6 +23,7 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.example.android.explorationgpa.data.ExplorationContract.SemesterGpaEntry;
 import com.example.android.explorationgpa.settings.SettingsActivity;
@@ -155,10 +156,17 @@ public class GpaActivity extends AppCompatActivity implements LoaderManager.Load
                 // get the semester uris (location inside database)for the selected semester items.
                 ArrayList<Uri> semesterUris = mSemesterCursorAdapter.getSemesterSelectedUris();
 
-                // open the CumulativeGpaActivity by intent contain the semester uris.
-                Intent intent = new Intent(GpaActivity.this, CumulativeGpaActivity.class);
-                intent.putExtra("semester_uris", semesterUris);
-                startActivity(intent);
+                // Check if the semesters that the user selected valid to be calculate the cumulative for it or not.
+                boolean hasValidSemester = checkSemesterUrisValidate(semesterUris);
+
+                // start the CumulativeActivity to show the cumulative gpa.
+                if (hasValidSemester) {
+                    // open the CumulativeGpaActivity by intent contain the semester uris.
+                    Intent intent = new Intent(GpaActivity.this, CumulativeGpaActivity.class);
+                    intent.putExtra("semester_uris", semesterUris);
+                    startActivity(intent);
+                }
+
 
             }
         });
@@ -347,6 +355,127 @@ public class GpaActivity extends AppCompatActivity implements LoaderManager.Load
             }
         });
 
+    }
+
+
+    /**
+     * Check if the semesters that the user selected valid to be calculate the cumulative for it or not.
+     * Valid semesters means that the minimum selected semester must be 2 and the maximum must be 10
+     * and the user can't select two semesters with the same number.
+     *
+     * @param semesterUris array contain the uris for the semesters refer to the location in database.
+     *
+     * @return boolean value => true mean no problem with the selected semester - false the opposite.
+     */
+    private boolean checkSemesterUrisValidate(ArrayList<Uri> semesterUris) {
+
+        // check all validation.
+        if (semesterUris.size() < 2) {
+
+            // if the user select less than 2 semester, a toast message will appear says "select at least 2 semesters".
+            Toast.makeText(GpaActivity.this, R.string.minimum_semester_selected, Toast.LENGTH_SHORT).show();
+
+            // quit from method early by false value cause there is problem in uris inserted.
+            return false;
+
+        } else if (semesterUris.size() > 10) {
+
+            // if the user select more than 10 semester, a toast message will appear says "select 10 semesters maximum".
+            Toast.makeText(GpaActivity.this, R.string.maximum_semester_selected, Toast.LENGTH_SHORT).show();
+
+            // quit from method early by false value cause there is problem in uris inserted.
+            return false;
+
+        } else {
+
+            // check if there is a semester repeated twice or more.
+            boolean hasRepeatedSemester = isSimilarity(semesterUris);
+
+            if (hasRepeatedSemester) {
+                // if the user select same semester, a toast message will appear says "don't repeat semesters".
+                Toast.makeText(GpaActivity.this, R.string.semester_repeated, Toast.LENGTH_SHORT).show();
+
+                // quit from method by false value cause there is problem in uris inserted.
+                return false;
+            }
+        }
+
+        // refer to that there is no problem in the uris inserted.
+        return true;
+
+    }
+
+
+    /**
+     * Check the semester numbers that inserted to discover if there is repeated semesters or not.
+     *
+     * @param uris array contain the uris for the semesters refer to the location in database.
+     *
+     * @return boolean value => true mean there is repeating in semesters - false the opposite.
+     */
+    private boolean isSimilarity(ArrayList<Uri> uris) {
+
+        // create the projection for the query method.
+        // all we need here the semester number.
+        String[] projection = { SemesterGpaEntry.COLUMN_SEMESTER_NUMBER };
+
+        // create a null Cursor object that will used the data come from the database.
+        Cursor cursor = null;
+
+        // initialize integer array that will contain the semester numbers.
+        ArrayList<Integer> semesterNumbers = new ArrayList<>();
+
+        // size of the uris array that inserted to the method.
+        int arraySize = uris.size();
+
+        // get all the semester numbers.
+        for (int i = 0 ; i < arraySize ; i++) {
+
+            // get a single uri from the array of the semester uris that inserted.
+            Uri currentUri = uris.get(i);
+
+            // get the semester info from the database.
+            cursor = getContentResolver().query(currentUri, projection, null, null, null);
+
+            // move to the valid column in the cursor.
+            cursor.moveToNext();
+
+            // get number of the column that store the semester number at.
+            int columnSemesterIndex = cursor.getColumnIndex(SemesterGpaEntry.COLUMN_SEMESTER_NUMBER);
+
+            // get the semester number.
+            int numberFromDatabase = cursor.getInt(columnSemesterIndex);
+
+            // add the semester number to the array that responsible to store all semester number inside it.
+            semesterNumbers.add(numberFromDatabase);
+
+        }
+
+        // to clear the resources that the app use after no need to the cursor.
+        cursor.close();
+
+
+
+        // check if there is repeat in the semester numbers or not.
+        for (int i = 0 ; i < arraySize ; i++) {
+
+            // get number of single semester.
+            int number = semesterNumbers.get(i);
+
+            // compare between the number above and other numbers in the array.
+            for (int j = 0 ; j < arraySize ; j++) {
+
+                // if there is similarity the method quit and return true (there is repeated).
+                if (number == semesterNumbers.get(j) && i != j) {
+                    return true;
+
+                }
+            }
+
+        }
+
+        // refer to that there is no repeated.
+        return false;
     }
 
 
