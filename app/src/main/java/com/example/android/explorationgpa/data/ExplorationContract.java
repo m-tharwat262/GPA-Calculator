96b298b4a8ef2763c6bfb6191a91fc02fe92ecb7
mahmoud-tharwat <mahmoudtharwat909090@gmail.com>
2021-05-15 01:40:45 +0200

    Add inner class in ExplorationContract.java to put the constant values to the new database that will be created to store the cumulative gpa item in the app

--- app/src/main/java/com/example/android/explorationgpa/data/ExplorationContract.java
+++ app/src/main/java/com/example/android/explorationgpa/data/ExplorationContract.java
@@ -1,22 +1,30 @@
 package com.example.android.explorationgpa.data;
 
+
 import android.content.ContentResolver;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
 
+
 public final class ExplorationContract {
 
+
     public static final String CONTENT_AUTHORITY = "com.example.android.explorationgpa"; // the content authority.
+
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY); // the uri for the database.
-    public static final String PATH_SEMESTER_GPA = "semester_gpa"; // table name for the semester gpa inside the uri path.
+
+    public static final String PATH_SEMESTER_GPA = "semester_gpa"; // table name for the semester gpa used in uri path.
+    public static final String PATH_CUMULATIVE_GPA = "cumulative_gpa"; // table name for the cumulative used in the uri path.
 
 
 
-    // Class For the semester total gpa table in the database.
+
+    // Class For the semester total gpa in the databases.
     public static final class SemesterGpaEntry implements BaseColumns {
 
-        // the uri for the semester total gpa table in the database.
+
+        // the uri contains the path to the semester gpa table in the databases.
         public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_SEMESTER_GPA);
 
         public final static String TABLE_NAME = "semester_gpa"; // table name.
@@ -77,5 +85,31 @@ public final class ExplorationContract {
 
 
     }
+
+
+
+
+    // Class For the cumulative gpa for the databases.
+    public static final class CumulativeGpaEntry implements BaseColumns {
+
+
+        // the uri contains the path to the cumulative gpa table in the databases.
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_CUMULATIVE_GPA);
+
+        public final static String TABLE_NAME = "cumulative_gpa"; // table name.
+
+
+
+        // head names for the table rows.
+        public final static String _ID = BaseColumns._ID; // unique id for each column.
+        public final static String COLUMN_STUDENT_NAME = "name"; // student name.
+        public final static String COLUMN_STUDENT_ID = "id"; // student id.
+        public final static String COLUMN_SEMESTER_URIS = "uris"; // semester uris refer to location (inside semester database).
+        public final static String COLUMN_UNIX = "unix"; // for the date and time.
+
+
+    }
+
+
 
 }
