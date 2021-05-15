4a9e6a3af579a9e292cadc6ee6430deeb76af3a8
mahmoud-tharwat <mahmoudtharwat909090@gmail.com>
2021-05-15 02:00:54 +0200

    Update the ExplorationProvider to make it handle access to the cumulative database by reading and writing actions
    handle actions (query - insert - delete).
    the cumulative database will not allow the user to update the cumulative items inside it so action update not setup in the ExplorationProvider.

--- app/src/main/java/com/example/android/explorationgpa/data/ExplorationProvider.java
+++ app/src/main/java/com/example/android/explorationgpa/data/ExplorationProvider.java
@@ -1,5 +1,6 @@
 package com.example.android.explorationgpa.data;
 
+
 import android.content.ContentProvider;
 import android.content.ContentUris;
 import android.content.ContentValues;
@@ -8,7 +9,10 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.util.Log;
+
 import com.example.android.explorationgpa.data.ExplorationContract.SemesterGpaEntry;
+import com.example.android.explorationgpa.data.ExplorationContract.CumulativeGpaEntry;
+
 
 
 public class ExplorationProvider extends ContentProvider {
@@ -18,8 +22,13 @@ public class ExplorationProvider extends ContentProvider {
 
 
     private SemesterGpaDbHelper mSemesterDbHelper; // get instance of the semester database.
-    private static final int SEMESTER_GPA = 100; // URI pattern to all the semester_gap table.
-    private static final int SEMESTER_GPA_ID = 101; // URI pattern to single column in the semester_gap table.
+    private CumulativeGpaDbHelper mCumulativeGpaDbHelper; // // get instance of the cumulative database.
+
+    private static final int SEMESTER_GPA = 100; // URI pattern to all the semester_gpa table.
+    private static final int SEMESTER_GPA_ID = 101; // URI pattern to single column in the semester_gpa table.
+
+    private static final int CUMULATIVE_GPA = 200; // URI pattern to all the cumulative_gpa table.
+    private static final int CUMULATIVE_GPA_ID = 201; // URI pattern to single column in the cumulative_gpa table.
 
 
     // initialize the UriMatcher.
@@ -29,6 +38,10 @@ public class ExplorationProvider extends ContentProvider {
     static {
         sUriMatcher.addURI(ExplorationContract.CONTENT_AUTHORITY, ExplorationContract.PATH_SEMESTER_GPA, SEMESTER_GPA);
         sUriMatcher.addURI(ExplorationContract.CONTENT_AUTHORITY, ExplorationContract.PATH_SEMESTER_GPA + "/#", SEMESTER_GPA_ID);
+
+        sUriMatcher.addURI(ExplorationContract.CONTENT_AUTHORITY, ExplorationContract.PATH_CUMULATIVE_GPA, CUMULATIVE_GPA);
+        sUriMatcher.addURI(ExplorationContract.CONTENT_AUTHORITY, ExplorationContract.PATH_CUMULATIVE_GPA + "/#", CUMULATIVE_GPA_ID);
+
     }
 
 
@@ -38,10 +51,12 @@ public class ExplorationProvider extends ContentProvider {
     @Override
     public boolean onCreate() {
 
-        // initialize semester database.
+        // initialize databases (semester - cumulative).
         mSemesterDbHelper = new SemesterGpaDbHelper(getContext());
+        mCumulativeGpaDbHelper = new CumulativeGpaDbHelper(getContext());
 
         return false;
+
     }
 
 
@@ -60,8 +75,9 @@ public class ExplorationProvider extends ContentProvider {
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
 
-        // access to the semester database to read the data from it.
+        // access to databases (semester - cumulative) to read the data from it.
         SQLiteDatabase semesterDatabase = mSemesterDbHelper.getReadableDatabase();
+        SQLiteDatabase cumulativeDatabase = mCumulativeGpaDbHelper.getReadableDatabase();
 
         // for the return result from the database.
         Cursor cursor;
@@ -72,7 +88,7 @@ public class ExplorationProvider extends ContentProvider {
         // setup functions for every uri pattern.
         switch (match) {
 
-            // semester_gpa database with no id.
+            // access to semester database with no id.
             case SEMESTER_GPA:
 
                 // setup the input inside the query function.
@@ -81,7 +97,7 @@ public class ExplorationProvider extends ContentProvider {
 
                 break;
 
-            // semester_gpa database with id.
+            // access to semester database with id.
             case SEMESTER_GPA_ID:
 
                 // setup the input inside the query function (after the WHERE word).
@@ -89,6 +105,27 @@ public class ExplorationProvider extends ContentProvider {
                 selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
 
                 cursor = semesterDatabase.query(SemesterGpaEntry.TABLE_NAME, projection, selection, selectionArgs,
+                        null, null, sortOrder);
+
+                break;
+
+            // access to cumulative database with no id.
+            case CUMULATIVE_GPA:
+
+                // setup the input inside the query function.
+                cursor = cumulativeDatabase.query(CumulativeGpaEntry.TABLE_NAME, projection,
+                        selection, selectionArgs, null, null, sortOrder);
+
+                break;
+
+            // access to cumulative database with id.
+            case CUMULATIVE_GPA_ID:
+
+                // setup the input inside the query function (after the WHERE word).
+                selection = CumulativeGpaEntry._ID + "=?";
+                selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
+
+                cursor = cumulativeDatabase.query(CumulativeGpaEntry.TABLE_NAME, projection, selection, selectionArgs,
                         null, null, sortOrder);
 
                 break;
@@ -125,12 +162,20 @@ public class ExplorationProvider extends ContentProvider {
         // setup functions for every uri pattern (no single uris by id).
         switch (match) {
 
-            // semester_gpa database.
+            // semester_gpa table inside semester database.
             case SEMESTER_GPA:
 
-                // execute helper method to insert the data inside the database.
-                // return the uri that refer to the row place inside the database.
+                // execute helper method to insert the data inside the semester database.
+                // return the uri that refer to the row place inside the semester database.
                 return insertSemesterGpa(uri, values);
+
+
+            // cumulative_gpa table inside cumulative database.
+            case CUMULATIVE_GPA:
+
+                // execute helper method to insert the data inside the cumulative database.
+                // return the uri that refer to the row place inside the cumulative database.
+                return insertCumulativeGpa(uri, values);
 
             // to handle if the is no match for the uri inserted with the uri patterns.
             default:
@@ -141,10 +186,10 @@ public class ExplorationProvider extends ContentProvider {
 
     /**
      * (Helper Method to Insert Data Inside semester Database).
-     * Insert data inside the semester gpa database.
+     * Insert data inside the semester_gpa table in semester database.
      * Check the validation for the data inserted from the user.
      *
-     * @param uri uri for the database path.
+     * @param uri uri for the path inside the database.
      * @param values contain the columns keys and its values.
      *
      * @return uri refer to the row place inside the database.
@@ -154,19 +199,19 @@ public class ExplorationProvider extends ContentProvider {
         // sanity chick for the student name before enter it inside the database.
         String studentName = values.getAsString(SemesterGpaEntry.COLUMN_STUDENT_NAME);
         if (studentName == null) {
-            throw new IllegalArgumentException("gpa item requires a name");
+            throw new IllegalArgumentException("gpa item for semester requires a name");
         }
 
         // sanity chick for the student ID before enter it inside the database.
         Integer studentCode = values.getAsInteger(SemesterGpaEntry.COLUMN_STUDENT_ID);
         if (studentCode == null || studentCode < 0) {
-            throw new IllegalArgumentException("gpa item requires valid id");
+            throw new IllegalArgumentException("gpa item for semester requires valid id");
         }
 
         // sanity chick for the semester number before enter it inside the database.
         Integer studentSemester = values.getAsInteger(SemesterGpaEntry.COLUMN_SEMESTER_NUMBER);
         if (studentSemester == null || !SemesterGpaEntry.isValidSemester(studentSemester)) {
-            throw new IllegalArgumentException("gpa item requires valid semester number");
+            throw new IllegalArgumentException("gpa item for semester requires valid semester number");
         }
 
 
@@ -180,7 +225,7 @@ public class ExplorationProvider extends ContentProvider {
         // make the uri return value equal null in case the insertion failed.
         if (id == -1) {
 
-            Log.e(LOG_TAG, "Failed to insert row for " + uri);
+            Log.e(LOG_TAG, "Failed to insert row in semester database for " + uri);
 
             return null;
         }
@@ -190,6 +235,56 @@ public class ExplorationProvider extends ContentProvider {
 
         // return the uri for the place that the data inserted inside database.
         return ContentUris.withAppendedId(uri, id);
+
+    }
+
+
+    /**
+     * (Helper Method to Insert Data Inside cumulative Database).
+     * Insert data inside the cumulative_gpa in cumulative database.
+     * Check the validation for the data inserted from the user.
+     *
+     * @param uri uri for the path inside the database.
+     * @param values contain the columns keys and its values.
+     *
+     * @return uri refer to the row place inside the database.
+     */
+    private Uri insertCumulativeGpa(Uri uri, ContentValues values) {
+
+        // sanity chick for the student name before enter it inside the database.
+        String studentName = values.getAsString(CumulativeGpaEntry.COLUMN_STUDENT_NAME);
+        if (studentName == null) {
+            throw new IllegalArgumentException("gpa item for cumulative requires a name");
+        }
+
+        // sanity chick for the student ID before enter it inside the database.
+        Integer studentCode = values.getAsInteger(CumulativeGpaEntry.COLUMN_STUDENT_ID);
+        if (studentCode == null || studentCode < 0) {
+            throw new IllegalArgumentException("gpa item for cumulative requires valid id");
+        }
+
+
+        // access to the database to write inside it.
+        SQLiteDatabase cumulativeDatabase = mCumulativeGpaDbHelper.getWritableDatabase();
+
+        // insert the data inside the database and get the row number that the data inserted at.
+        long id = cumulativeDatabase.insert(CumulativeGpaEntry.TABLE_NAME, null, values);
+
+        // if the insertion failed show a Log(e) for that.
+        // make the uri return value equal null in case the insertion failed.
+        if (id == -1) {
+
+            Log.e(LOG_TAG, "Failed to insert row in cumulative database for " + uri);
+
+            return null;
+        }
+
+        // Notify that there is changing happened in the database to sync changes to the network or activities.
+        getContext().getContentResolver().notifyChange(uri, null);
+
+        // return the uri for the place that the data inserted inside database.
+        return ContentUris.withAppendedId(uri, id);
+
     }
 
 
@@ -316,8 +411,10 @@ public class ExplorationProvider extends ContentProvider {
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
 
-        // access to the database to write inside it.
+        // access to databases (semester - cumulative) to write inside it.
         SQLiteDatabase semesterDatabase = mSemesterDbHelper.getWritableDatabase();
+        SQLiteDatabase cumulativeDatabase = mCumulativeGpaDbHelper.getWritableDatabase();
+
 
         // get the pattern that the uri equal.
         final int match = sUriMatcher.match(uri);
@@ -346,6 +443,28 @@ public class ExplorationProvider extends ContentProvider {
                 // delete the data from the database.
                 // return number of the rows that deleted from the database.
                 rowsDeleted = semesterDatabase.delete(SemesterGpaEntry.TABLE_NAME, selection, selectionArgs);
+                break;
+
+
+
+            // semester_gpa database with no id.
+            case CUMULATIVE_GPA:
+
+                // delete the data from the database.
+                // return number of the rows that deleted from the database.
+                rowsDeleted = cumulativeDatabase.delete(CumulativeGpaEntry.TABLE_NAME, selection, selectionArgs);
+                break;
+
+            // semester_gpa database with id.
+            case CUMULATIVE_GPA_ID:
+
+                // setup the input inside the update function (after the WHERE word).
+                selection = CumulativeGpaEntry._ID + "=?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+
+                // delete the data from the database.
+                // return number of the rows that deleted from the database.
+                rowsDeleted = cumulativeDatabase.delete(CumulativeGpaEntry.TABLE_NAME, selection, selectionArgs);
                 break;
 
             // to handle if the is no match for the uri inserted with the uri patterns.
