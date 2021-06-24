7e0122e79ac8b4711293cf4943dffa8790aec401
mahmoud-tharwat <mahmoudtharwat909090@gmail.com>
2021-06-24 23:26:41 +0200

    Add a new Activity to the setting package called SubjectInfoActivity
    - this activity will contain the subject info (hours & success degree).
    - this activity will do the HoursActivity & SuccessActivity work in one single activity.

---
+++ app/src/main/java/com/example/android/explorationgpa/settings/SubjectInfoActivity.java
@@ -0,0 +1,18 @@
+package com.example.android.explorationgpa.settings;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.android.explorationgpa.R;
+
+public class SubjectInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting_subject_info);
+
+    }
+
+}
